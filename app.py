1b5074d5be04f2c633e8af2bdc508a3bb27b0b0a
dixiemarielaput <50008340+dixiemarielaput@users.noreply.github.com>
2023-07-25 07:06:21 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "hello there"
 
-if __name__ =="__main__":
-    app.run(host='0.0.0.0', debug=True)
+@app.route("/")
+def hello_webint():
+  return render_template('home.html')
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
